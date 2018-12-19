9dcefdb7a763e49a83a402a995421c984073317c
liujia <384369178@qq.com>
2018-12-19 15:29:48 +0800

    this is second commit

---
+++ StringTools/src/test/java/com/jiafei/test/StringTools/StringToolsTest.java
@@ -0,0 +1,16 @@
+package com.jiafei.test.StringTools;
+
+import org.junit.Test;
+
+/**
+ * @Descripton: TODO
+ * @Author: jiafeiblog.com
+ * @Date: 2018/12/19
+ * @Since: jdk1.8
+ */
+public class StringToolsTest {
+	@Test
+	public void test1(){
+		System.out.println("this is test code");
+	}
+}
